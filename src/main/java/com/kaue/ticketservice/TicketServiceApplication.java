e120404694edc095350d3b8e4a5953e6b9f4b2f3
Kauê Gatto <kaue.gatto@ab-inbev.com>
2023-05-05 13:12:52 -0300

    initial commit - spring

---
+++ src/main/java/com/kaue/ticketservice/TicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaue.ticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceApplication.class, args);
+	}
+
+}
