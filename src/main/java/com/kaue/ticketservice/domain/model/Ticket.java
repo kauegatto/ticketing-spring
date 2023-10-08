62362e96f25c765e6334399b2f9f315abb265914
kaue gatto <kauegatto123@gmail.com>
2023-10-08 20:13:07 -0300

    refactor: add final

--- src/main/java/com/kaue/ticketservice/domain/model/Ticket.java
+++ src/main/java/com/kaue/ticketservice/domain/model/Ticket.java
@@ -4,23 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
 
 public class Ticket {
     @Schema(description = "User email")
-    private String requesterEmail;
+    final private String requesterEmail;
     @Schema(description = "Assignee / Agent email")
     private Long assigneeEmail;
     @Schema(description = "Title of the ticket")
-    private String title;
+    final private String title;
     @Schema(description = "Ticket description")
     private String description;
     @CreatedDate
     @Schema(description = "Create date")
-    private Instant createDate;
+    final private Instant createDate;
     @Getter @Setter @LastModifiedDate
     private Instant  updatedDate;
     @Schema(description = "Resolution date")
