fd89563ff39a1c2144d783366378d7a5af147929
kaue gatto <kauegatto123@gmail.com>
2023-10-09 17:58:17 -0300

    fix: I still need mongo auto configuration :(, fix host

--- src/main/java/com/kaue/ticketservice/TicketServiceApplication.java
+++ src/main/java/com/kaue/ticketservice/TicketServiceApplication.java
@@ -3,9 +3,8 @@ package com.kaue.ticketservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude={MongoAutoConfiguration.class})
+@SpringBootApplication()
 @Slf4j
 public class TicketServiceApplication {
 	public static void main(String[] args) {
