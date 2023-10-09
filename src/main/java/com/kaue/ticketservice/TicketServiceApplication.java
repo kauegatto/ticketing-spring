716b7d4d935de67f72acbc20c56c38e4fcdb0c3c
kaue gatto <kauegatto123@gmail.com>
2023-10-09 17:54:43 -0300

    fix: fix env name, disable mongo auto configuration

--- src/main/java/com/kaue/ticketservice/TicketServiceApplication.java
+++ src/main/java/com/kaue/ticketservice/TicketServiceApplication.java
@@ -1,13 +1,15 @@
 package com.kaue.ticketservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
+@Slf4j
 public class TicketServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(TicketServiceApplication.class, args);
+		SpringApplication.run(TicketServiceApplication.class, args); log.info("Started!");
 	}
 
 }
