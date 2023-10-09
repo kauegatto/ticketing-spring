b0968e8bb0e415ddf366c17293ccebcd2cc3a7ab
kaue gatto <kauegatto123@gmail.com>
2023-10-09 20:01:00 -0300

    fix: fix createDate initdb

--- scripts/docker/ticketing_data.js
+++ scripts/docker/ticketing_data.js
@@ -4,11 +4,6 @@ db.getSiblingDB('ticketing-service').ticket.insertMany([
     "assigneeEmail":"example@agent.com",
     "title":"Pls help me",
     "description":"How do i open a support request",
-    "createDate": {
-           "$date": "2023-10-08T22:19:31.605Z"
-   },
-    "createDate": {
-           "$date": "2023-10-08T22:19:31.605Z"
-    },
+    "createDate": "2023-10-09T22:56:52.175+00:00"
   }
 ]);
