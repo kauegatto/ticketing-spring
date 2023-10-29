9c3453f4566e626a13bed0af69e7bc54d656e795
kaue gatto <kauegatto123@gmail.com>
2023-10-29 02:31:49 -0300

    feat: add Id

--- ticket-service/src/main/java/com/kaue/ticketservice/application/dto/TicketResponseDTO.java
+++ ticket-service/src/main/java/com/kaue/ticketservice/application/dto/TicketResponseDTO.java
@@ -1,5 +1,7 @@
 package com.kaue.ticketservice.application.dto;
 
+import com.kaue.ticketservice.domain.model.state.TicketStatusState.State;
+
 import java.time.Instant;
 
-public record TicketResponseDTO (String title, String description, Instant createDate, String requesterEmail){}
+public record TicketResponseDTO (String id,String title, String description, Instant createDate, String requesterEmail, State state){}
