59c440f753806a3da84c7fd59b34bb8b41d5d0f1
kaue gatto <kauegatto123@gmail.com>
2024-03-27 16:18:40 -0300

    refactor: global setter

--- ticket-service/src/main/java/com/kaue/ticketservice/domain/model/Ticket.java
+++ ticket-service/src/main/java/com/kaue/ticketservice/domain/model/Ticket.java
@@ -8,32 +8,28 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 
+@Getter
 @ToString
 @AllArgsConstructor
 public class Ticket {
-    @Getter
     final private String id;
     @Schema(description = "User email")
-    @Getter
     final private String requesterEmail;
     @Schema(description = "Assignee / Agent email")
-    @Getter
     private String assigneeEmail;
     @Schema(description = "Title of the ticket")
-    @Getter
     final private String title;
     @Schema(description = "Ticket description")
-    @Getter @Setter
+    @Setter
     private String description;
-    @Getter @Setter @Schema(description = "Ticket state, follows specific state machine found on docs")
+    @Setter @Schema(description = "Ticket state, follows specific state machine found on docs")
     private State state; // todo rethink about getters and setters here, also shouldn't each ticket have a state machine?
     @Schema(description = "Create date")
-    @Getter
     final private Instant createDate;
-    @Getter @Setter @LastModifiedDate
+    @Setter @LastModifiedDate
     private Instant  updatedDate; // todo remove it
     @Schema(description = "Resolution date")
-    @Getter @Setter
+    @Setter
     private OffsetDateTime resolutionDate;
 
     public void setAssignee(String assigneeEmail){
